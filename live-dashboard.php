0b0e69ab2b17f9bf155dba4a0513c07ad8908368
mike <mike@localhost>
2013-01-06 11:41:17 +0200

    Version bump

--- live-dashboard.php
+++ live-dashboard.php
@@ -3,7 +3,7 @@
   Plugin Name: Live Dashboard
   Plugin URI: http://trenvo.com/wordpress-live-dashboard
   Description: Manage your website while you're browsing it.
-  Version: 0.1.1
+  Version: 0.1.2
   Author: Mike Martel
   Author URI: http://trenvo.com
  */
